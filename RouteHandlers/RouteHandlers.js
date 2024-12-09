06e8357f30effcf33e57115fed816d83d24a9cf9
AbdulSamadNadeem <samadnadeem372@gmail.com>
2024-12-09 14:13:20 +0500

    updated

--- RouteHandlers/RouteHandlers.js
+++ RouteHandlers/RouteHandlers.js
@@ -37,7 +37,7 @@ exports.getDocById = async(req,res)=>{
 
 exports.CreateDoc = async(req,res)=>{
       try{
-        const Movies = MovieModel.create(req.body)
+        const Movies = await MovieModel.create(req.body)
         res.status(201).json({
             status :"success",
             data : {
@@ -54,7 +54,7 @@ exports.CreateDoc = async(req,res)=>{
 
 exports.UpdateDoc =async(req,res)=>{
     try{
-        const Movies = MovieModel.findByIdAndUpdate(req.params.id , req.body , {new:true , runValidators:true})
+        const Movies = await MovieModel.findByIdAndUpdate(req.params.id , req.body , {new:true , runValidators:true})
         res.status(200).json({
             status :"success",
             data : {
